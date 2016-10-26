aad05e7993e73d9a8ead715161d0deeaff2030e0
Ben Edwards <ben.edwards@ontology.com>
2016-10-26 14:05:19 +0100

    term caps don't have to be universal vars

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -20,12 +20,12 @@ set fish_color_end         bbbbbb
 set fish_auto_suggestion   666666
 
 # dem sweet manpage colors
-set -xU LESS_TERMCAP_md (printf "\e[01;31m")
-set -xU LESS_TERMCAP_me (printf "\e[0m")
-set -xU LESS_TERMCAP_se (printf "\e[0m")
-set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
-set -xU LESS_TERMCAP_ue (printf "\e[0m")
-set -xU LESS_TERMCAP_us (printf "\e[01;32m")
+set -x LESS_TERMCAP_md (printf "\e[01;31m")
+set -x LESS_TERMCAP_me (printf "\e[0m")
+set -x LESS_TERMCAP_se (printf "\e[0m")
+set -x LESS_TERMCAP_so (printf "\e[01;44;33m")
+set -x LESS_TERMCAP_ue (printf "\e[0m")
+set -x LESS_TERMCAP_us (printf "\e[01;32m")
 
 . "$HOME/.homesick/repos/homeshick/homeshick.fish"
 . "$HOME/.homesick/repos/homeshick/completions/homeshick.fish"
